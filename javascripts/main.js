b8a7c5e473d11f506cecba520a31d4afd0620bc9
Agustin Falco <afalco@sophilabs.com>
2016-11-03 12:10:21 -0300

    Added animation for text.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,13 @@
-console.log('This would be the main JS file.');
+makeTextDraggable('title', 50);
+makeTextDraggable('description', 10);
+makeTextDraggable('headerTitle', 50);
+function makeTextDraggable(name, ms) {
+  var titleText = document.getElementById(name);
+  var result = ''
+  for (var i = 0; i < titleText.innerHTML.length; i ++) {
+  let current = titleText.innerHTML[i];
+    current = '<span class="draggable" style="animation-delay:'+ ms * i +'ms;">' + current + '</span>';
+    result += current;
+  }
+  titleText.innerHTML = result;
+}
