b18ab4b26d62e80cee6cbc7e1c68a60de4a47a88
Agustin Falco <afalco@sophilabs.com>
2016-11-03 20:08:48 -0300

    Added loader to prevent showing animations before image is finished loading.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,8 +1,16 @@
-makeTextDraggable('title', 50);
-makeTextDraggable('description', 10);
-makeTextDraggable('headerTitle', 50);
+$('<img/>').attr('src', 'images/forest.jpg')[0].onload = function() {
+  debugger;
+  $(this).remove(); // prevent memory leaks as @benweet suggested
+  var loader = document.getElementsByClassName('loader')[0].style.display = 'none';
+  document.getElementById('bodySection').removeAttribute('hidden');
+  makeTextDraggable('#title', 50);
+  makeTextDraggable('#description', 10);
+  makeTextDraggable('#headerTitle', 50);
+};
+
+
 function makeTextDraggable(name, ms) {
-  var titleText = document.getElementById(name);
+  var titleText = $(name);
   var result = ''
   for (var i = 0; i < titleText.innerHTML.length; i ++) {
   let current = titleText.innerHTML[i];
