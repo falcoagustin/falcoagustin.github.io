f8d483be2b427adc89e0ea77d2ba7aebb4b753e2
Agustin Falco <afalco@sophilabs.com>
2016-11-06 12:49:40 -0300

    Removed powerpoint effects and added typed.js gem to the project to write text.

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,9 +2,9 @@ $('<img/>').attr('src', 'images/forest.jpg')[0].onload = function() {
   $(this).remove(); // prevent memory leaks as @benweet suggested
   $('.loader')[0].style.display = 'none';
   $('#bodySection')[0].removeAttribute('hidden');
-  makeTextDraggable('#title', 50);
-  makeTextDraggable('#description', 10);
-  makeTextDraggable('#headerTitle', 50);
+  // makeTextDraggable('#title', 50);
+  // makeTextDraggable('#description', 10);
+  // makeTextDraggable('#headerTitle', 50);
 };
 
 
@@ -23,3 +23,37 @@ function makeTextDraggable(name, ms) {
   }
   titleText.innerHTML = result;
 }
+
+function callTyped(target, string) {
+  $(target).typed({
+    strings: [string],
+    typeSpeed: 30,
+    loop: false,
+    contentType: 'html', // or text
+    callback: function(){
+
+      $(".typed-cursor").remove();
+      $("#title").typed({
+        strings: ["Greetings! I'm Agustín^500"],
+        typeSpeed: 30,
+        loop: false,
+        contentType: 'html', // or text
+        callback: function(){
+          $(".typed-cursor").remove();
+          $("#description").typed({
+            strings: ["Programer by definition, if you have something interesting to code, what are you waiting for? =) "],
+            typeSpeed: 20,
+            loop: false,
+            contentType: 'html', // or text
+            callback: function(){
+            },
+            resetCallback: function() { }
+          });
+        },
+        resetCallback: function() { }
+      });
+    },
+    resetCallback: function() { }
+  });
+}
+callTyped("#mainTitle", "Agustín Falco Developer^500");
