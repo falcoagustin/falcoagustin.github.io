056ccbf9ac34f13f8e20c2e667c2550af03c4a73
Agustin Falco <agusfalco_11@hotmail.com>
2017-07-29 18:58:34 -0300

    Restyled site with new animations.

--- javascripts/main.js
+++ javascripts/main.js
@@ -34,26 +34,27 @@ function makeTextDraggable(name, ms) {
 function callTyped(target, string) {
   $(target).typed({
     strings: [string],
-    typeSpeed: 30,
+    typeSpeed: 15,
     loop: false,
     contentType: 'html', // or text
     callback: function(){
 
       $(".typed-cursor").remove();
       $("#title").typed({
-        strings: ["Greetings! I'm Agustín^1000"],
-        typeSpeed: 30,
+        strings: ["Greetings! I'm Agustín^700"],
+        typeSpeed: 10,
         loop: false,
         contentType: 'html', // or text
         callback: function(){
           $(".typed-cursor").remove();
           $("#description").typed({
-            strings: ["Programer by definition,^500 if you have something interesting to code,^250 what are you waiting for? =) "],
-            typeSpeed: 20,
+            strings: ["Programer by definition,^250 if you have something interesting to code,^125 what are you waiting for? =) "],
+            typeSpeed: 5,
             loop: false,
             contentType: 'html', // or text
             callback: function(){
               $('.media-options').addClass('active-smoked-container');
+              beginLinkSequence();
             },
             resetCallback: function() { }
           });
@@ -63,4 +64,21 @@ function callTyped(target, string) {
     },
     resetCallback: function() { }
   });
+}
+
+function beginLinkSequence() {
+  var githubContainer = $('.github-container').addClass('animation-left');
+  githubContainer.one('webkitAnimationEnd oanimationend msAnimationEnd animationend',
+    function(e) {
+      var linkedinContainer = $('.linkedin-container').addClass('animation-right');
+      linkedinContainer.one('webkitanimationend oanimationend msanimationend animationend',
+        function(e){
+          var twitterContainer = $('.twitter-container').addClass('animation-left');
+          twitterContainer.one('webkitanimationend oanimationend msanimationend animationend',
+            function(e) {
+              var emailContainer = $('.email-container').addClass('animation-right');
+            });
+        });
+
+    });
 }
